710da1ace76fe1cabd14d8275060084ac70feb45
Carlos Coelho <carlos.neto.coelho@outlook.pt>
2023-11-28 16:31:21 +0000

    created validations and signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+  
   def show
     @user = current_user
     @group = current_user.groups
     @members = @group.members
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :first_name, :last_name, :nickname, :age, :location, :gender, :description)
   end
 end
