5efa2cbde4640de42b1ab725e8c4441e8b6e523b
Carlos Coelho <carlos.neto.coelho@outlook.pt>
2023-11-28 12:52:28 +0000

    created validations v1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :nickname, presence: true, uniqueness: true
+  validates :age, presence: true, numericality: { only_integer: true }
+  validates :location, presence: true
+  validates :description, presence: true, length: { maximum: 150 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
