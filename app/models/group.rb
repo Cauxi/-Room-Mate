9367465021140e70a2059837cf8d2aa6477fa159
Carlos Coelho <carlos.neto.coelho@outlook.pt>
2023-11-30 11:50:06 +0000

    can search for location and tags, and group name

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,12 @@
 class Group < ApplicationRecord
+  # include PgSearch::Model
+  # pg_search_scope :search_by_name,
+  #   against: [:name],
+  #   using: {
+  #     tsearch: { prefix: true }
+  #   }
+  # #
   belongs_to :user
   has_many :members
-
   validates :name, presence: true
 end
